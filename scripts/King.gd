726bca63cf1acd727ba4faead05a042402847144
Giovanni Abeni <giovanni.abeni.santos@usp.br>
2020-09-18 05:03:11 -0300

    movement, animations and camera starts

---
+++ scripts/King.gd
@@ -0,0 +1,87 @@
+extends KinematicBody
+
+### GLOBAL CONSTANTS ###
+const GRAVITY = -9.8
+const SPEED = 6
+const ACCELERATION = 3
+const DEACCELERATION = 5
+
+### STATEFUL PROPERTIES ###
+var velocity = Vector3()
+var is_moving = false
+
+### RELATED NODES ###
+var king: KinematicBody
+var camera
+var anim_tree: AnimationTree
+var anim_player: AnimationPlayer
+
+func _ready():
+	anim_tree = get_node("AnimationTree")
+	king = get_node(".")
+
+func _process(_delta):
+	pass
+
+func _physics_process(delta):
+	camera = get_node("Target/Camera").get_global_transform()
+
+	self._move_king(delta)
+	
+	
+func _move_king(delta):
+	
+	var dir = Vector3()
+
+	is_moving = false
+
+	if(Input.is_action_pressed("move_fw")):
+		dir += -camera.basis[2]
+		is_moving = true
+	
+	if(Input.is_action_pressed("move_bw")):
+		dir += camera.basis[2]
+		is_moving = true
+	
+	if(Input.is_action_pressed("move_l")):
+		dir += -camera.basis[0]
+		is_moving = true
+	
+	if(Input.is_action_pressed("move_r")):
+		dir += camera.basis[0]
+		is_moving = true
+	
+	dir.y = 0	
+	dir = dir.normalized()
+	
+	velocity.y += delta * GRAVITY
+
+	var hv = velocity # horizontal velocity
+	hv.y = 0
+	var new_pos = dir * SPEED
+	var accel = DEACCELERATION
+	
+	
+	if (dir.dot(hv) > 0):\
+		accel = ACCELERATION
+	
+	hv = hv.linear_interpolate(new_pos, accel * delta)	
+	
+	velocity.x = hv.x
+	velocity.z = hv.z
+		
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
+	
+	if is_moving:
+		
+		# Rotate the character
+		var angle = atan2(hv.x, hv.z)
+		
+		var char_rot = king.get_rotation()
+		
+		char_rot.y = angle
+		king.rotation = char_rot
+		
+	
+	var speed = hv.length() / SPEED
+	anim_tree.set('parameters/idle-walk-run/blend_amount', speed)
