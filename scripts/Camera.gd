4dc8fb18135c98421184323f8c73ac38107e93e5
h7seiji <helder_seiji@hotmail.com>
2020-09-18 19:36:13 -0300

    camera smooth

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -3,12 +3,14 @@ extends Camera
 # class member variables go here:
 export var distance = 8.0
 export var height = 4.0
+var smooth_pos_speed = 10
+var smooth_rot_speed = 6
+var smooth_camera_pos = Vector3()
+var smooth_camera_rot = Vector3()
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	set_physics_process(true)
-	
 	set_as_toplevel(true)
 
 func _physics_process(delta):
@@ -23,4 +25,7 @@ func _physics_process(delta):
 	
 	pos = target + offset
 	
-	look_at_from_position(pos, target, up)
+	smooth_camera_pos = smooth_camera_pos.linear_interpolate(pos, smooth_pos_speed * delta)
+	smooth_camera_rot = smooth_camera_rot.linear_interpolate(target, smooth_rot_speed * delta)
+	
+	look_at_from_position(smooth_camera_pos, smooth_camera_rot, up)
