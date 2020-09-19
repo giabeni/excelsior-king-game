e4a10d7df41e0df54bde0935d6d9d7b936cae0a3
papukweh <nathalia.borin@usp.br>
2020-09-19 11:54:31 -0300

    Adds cracking sound to falling platform

--- scripts/FallingPlatform.gd
+++ scripts/FallingPlatform.gd
@@ -3,6 +3,7 @@ extends KinematicBody
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
 		$FallTimer.start()
+		$AudioStreamPlayer.play()
 
 func _on_RespawnTimer_timeout():
 	self.show()
