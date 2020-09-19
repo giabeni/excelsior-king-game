4d6ab27c1cd50dbdb208edcbdb0da320d2edf3bf
papukweh <nathalia.borin@usp.br>
2020-09-19 14:37:24 -0300

    Makes stalactite fall

--- scripts/Stalactite.gd
+++ scripts/Stalactite.gd
@@ -1,6 +1,19 @@
-extends Area
+extends KinematicBody
+
+onready var velocity = Vector3(0,0,0)
+onready var falling = false
+const gravity = -15
+
+func _process(delta):
+	if falling:
+		velocity.y += delta * gravity
+		velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+	if $RayCast.is_colliding():
+		var thing = $RayCast.get_collider()
+		if thing.is_in_group("Player"):
+			falling = true
 
 
-func _on_Stalactite_body_entered(body):
+func _on_Area_body_entered(body):
 	if body.has_method("damage"):
 		body.damage(1)
