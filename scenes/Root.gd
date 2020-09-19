b556d7499340ab6618e7a9617ceb65dfbfa07755
papukweh <nathalia.borin@usp.br>
2020-09-19 17:38:17 -0300

    Adds controls to intro message

--- scenes/Root.gd
+++ scenes/Root.gd
@@ -7,6 +7,9 @@ func _ready():
 		yield(get_tree().create_timer(5), "timeout")
 		get_node("IntroUI/Part1").hide()
 		get_node("IntroUI/Part2").show()
+		yield(get_tree().create_timer(5), "timeout")
+		get_node("IntroUI/Part2").hide()
+		get_node("IntroUI/Part3").show()
 		global.played_intro = true
 		yield(get_tree().create_timer(5), "timeout")
 		get_node("IntroUI").hide()
